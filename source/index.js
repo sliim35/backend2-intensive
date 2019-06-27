91132371500204c8e663be172dab4d931d7e2492
Andrey Prisniak <aprisniak@lectrum.io>
2019-06-27 21:58:20 +0300

    feat(source): add middlewares

--- source/index.js
+++ source/index.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 // Instruments
 import { app } from './server';
 import { getPort } from './utils';
@@ -6,6 +8,8 @@ import { getPort } from './utils';
 import { users } from './routers';
 
 const PORT = getPort();
+
+app.use(express.json({ limit: '10kb' }));
 
 app.use('/users', users);
 
--- source/routers/users/index.js
+++ source/routers/users/index.js
@@ -4,11 +4,15 @@ import express from 'express';
 // Instruments
 import { get, post } from './route';
 import { getByHash } from './hash/route';
+import { limiter, validator } from '../../utils';
+
+// Schema
+import { createUser } from '../../schemas';
 
 export const router = express.Router();
 
-router.get('/', get);
-router.post('/', post);
+router.get('/', [ limiter(5, 60 * 1000) ], get);
+router.post('/', [ validator(createUser) ], post);
 
 router.get('/:userHash', getByHash);
 
