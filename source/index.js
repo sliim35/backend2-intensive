abde7a79f3b60fd7c5354d17f7a5e4387552a3ac
Andrey Prisniak <aprisniak@lectrum.io>
2019-07-14 19:03:19 +0300

    feat(source): refactor application

--- source/index.js
+++ source/index.js
@@ -1,37 +1,12 @@
-import express from 'express';
+// Core
 import dg from 'debug';
 
 // Instruments
+import { getPort } from './utils';
 import { app } from './server';
-import { getPort, logger } from './utils';
-
-// Routers
-import { auth, users, classes, lessons } from './routers';
 
 const debug = dg('server:main');
 const PORT = getPort();
-
-app.use(express.json({ limit: '10kb' }));
-
-// Logger
-if (process.env.NODE_ENV === 'development') {
-    app.use((req, res, next) => {
-        let body = null;
-
-        if (req.method !== 'GET') {
-            body = JSON.stringify(req.body, null, 2);
-        }
-
-        logger.debug(`${req.method} ${body ? `\n${body}` : ''}`);
-        next();
-    });
-}
-
-// Routers
-app.use('/api', auth);
-app.use('/api/users', users);
-app.use('/api/classes', classes);
-app.use('/api/lessons', lessons);
 
 app.listen(PORT, () => {
     debug(`Server API is up on port ${PORT}`);
--- source/server.js
+++ source/server.js
@@ -1,6 +1,34 @@
 // Core
 import express from 'express';
 
+// Instruments
+import { logger } from './utils';
+
+// Routers
+import { auth, users, classes, lessons } from './routers';
+
 const app = express();
+
+app.use(express.json({ limit: '10kb' }));
+
+// Logger
+if (process.env.NODE_ENV === 'development') {
+    app.use((req, res, next) => {
+        let body = null;
+
+        if (req.method !== 'GET') {
+            body = JSON.stringify(req.body, null, 2);
+        }
+
+        logger.debug(`${req.method} ${body ? `\n${body}` : ''}`);
+        next();
+    });
+}
+
+// Routers
+app.use('/api', auth);
+app.use('/api/users', users);
+app.use('/api/classes', classes);
+app.use('/api/lessons', lessons);
 
 export { app };
