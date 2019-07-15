6506e9f3bb8434ef073e7a1720c5fead57113bcd
Andrey Prisniak <aprisniak@lectrum.io>
2019-07-15 13:41:51 +0300

    feat(source): add solution lesson-11-task-1

--- source/server.js
+++ source/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 
 // Instruments
-import { logger } from './utils';
+import { logger, errorLogger } from './utils';
 
 // Routers
 import { auth, users, classes, lessons } from './routers';
@@ -30,5 +30,18 @@ app.use('/api', auth);
 app.use('/api/users', users);
 app.use('/api/classes', classes);
 app.use('/api/lessons', lessons);
+
+if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-unused-vars
+    app.use((error, req, res, next) => {
+        const { name, message, statusCode } = error;
+        const errorMessage = `${name}: ${message}`;
+
+        errorLogger.error(errorMessage);
+
+        const status = statusCode ? statusCode : 500;
+        res.status(status).json({ message: message });
+    });
+}
 
 export { app };
